76ab4012d5b316b16ba6ff6504dea7061f6325f5
박 종훈 <m05214@naver.com>
2020-12-18 23:36:17 +0900

    이력관련 컨트롤러 생성
    이력관련 컨트롤러 생성

---
+++ src/main/java/me/parkprin/careermanagementsystem/controller/resume/ResumeController.java
@@ -0,0 +1,9 @@
+package me.parkprin.careermanagementsystem.controller.resume;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resume/api")
+public class ResumeController {
+}
